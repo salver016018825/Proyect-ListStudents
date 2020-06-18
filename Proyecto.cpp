4d178093079fadd55dc65d32e18134df0d4dbb1e
Salver DLCC <salver.osorio@outlook.com>
2020-06-17 21:26:09 -0500

    Declaracion de Variables

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -34,7 +34,7 @@ int main(int argc, char const *argv[])
     cout<<"enter student lastName: ";
     cin.getline(lastname,19,'\n');
     
-    
+    //uso de 8 variables para este caso
     
     
     return 0;
