d9409c3d54cc7f2112ac25c6cb364a2b46254de5
Salver DLCC <salver.osorio@outlook.com>
2020-06-23 18:14:56 -0500

    Se muestra lista de alumnos con alumno que tiene nota mas baja y alta

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -1,5 +1,7 @@
 /*
-    Crea un programa que ingrese los datos de 20 alumnos, nombre, apellido, nombre de materia 1, calificación materia 1, nombre de materia 2, calificación materia 2, nombre de materia 3, calificación materia 3, nombre de materia 4, calificación materia 4, nombre de materia 5, calificación materia 5. Y después calcular el promedio general de cada alumno, y promedio final del grupo de 20 alumnos.
+    Crea un programa que ingrese los datos de 20 alumnos, nombre, apellido, nombre de materia 1, calificación materia 1, nombre de materia 2, 
+    calificación materia 2, nombre de materia 3, calificación materia 3, nombre de materia 4, calificación materia 4, nombre de materia 5, 
+    calificación materia 5. Y después calcular el promedio general de cada alumno, y promedio final del grupo de 20 alumnos.
 
     Mostrar en pantalla.
 
@@ -9,7 +11,8 @@
         Materia con menor calificación.
 
     Y al final el promedio general del grupo.
-    Una vez que se muestre en pantalla, darle la opción al usuario si todos los datos están correctos, y de no ser así se pueda modificar el dato erróneo y posteriormente mostrar de nuevo los datos en pantalla ya con los datos correctos.
+    Una vez que se muestre en pantalla, darle la opción al usuario si todos los datos están correctos, y de no ser así se pueda 
+    modificar el dato erróneo y posteriormente mostrar de nuevo los datos en pantalla ya con los datos correctos.
 
 */
 
@@ -21,10 +24,10 @@ using namespace std;
 int main(int argc, char const* argv[])
 {
     //Mys variables 
+      
 
     //array materias
-    const char listMaterias[5][20] = { "Matematicas","Geografia","Historia","Programacion","Filosofia"};
-    
+    const char listMaterias[5][20] = { "Matematicas","Geografia","Historia","Programacion","Filosofia"};    
 
     //areglo de 5 posiciones para las calificaciones 
     const int totalCalicaciones = 5;
@@ -35,78 +38,104 @@ int main(int argc, char const* argv[])
     //variables para sacar promedio gruipal
 
     int j = 0;
+    //int temp = 0;
     double sumaCalificacionPromedioAlumno = 0.0;
     double promedioGrupo = 0.0;
-    double listNotes[3] = {0,0};
+    double listNotes[20] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    /*double listNotes[3] = { 0,0,0 };*/
 
     // datos para un solo alumno
-    char name[3][10] = {};
-    char lastname[3][10] = {};
+    char name[20][20] = {};
+    char lastname[20][30] = {};
+    /*char name[3][20] = {};
+    char lastname[3][30] = {};*/
     double promedioAlumno = 0.0;
     double sumaNotas = 0.0;
 
+    //validacion
+    char respuesta = 'n';
+
     /*enter name and lastname in variable type char */
     cout << "enter data student" << endl;
-    for (int i = 0; i < 3; i++)
-    {
-        cout << "enter name: ";
-        cin >> name[i];
-        cout << "enter lastname: ";
-        cin >> lastname[i];
-        
-        //enter calification from student by input
-        cout << "enter calification from student " << endl;
-        for (int i = 0; i <= 4; i++)
+    
+        for (int i = 0; i < 20; i++)
         {
-            cout << "Enter "<< listMaterias[i] << " : ";
-            cin >> calificaciones[i];
-            sumaNotas += calificaciones[i];
-            promedioAlumno = sumaNotas / 5;
-            //el ultimo posicion es para el promedio del alumno
-            if (i == 4) 
+            do
             {
-                listNotes[j] = promedioAlumno;
-                sumaCalificacionPromedioAlumno += listNotes[j];
-                promedioGrupo = sumaCalificacionPromedioAlumno / 3;
-                promedioAlumno = 0.0;
-                sumaNotas = 0.0;
-                j++;
-            }
-        }
-    }
+                cout << "Student " << i + 1 << "\nenter name: ";
+                cin >> name[i];
+                cout << "enter lastname: ";
+                cin >> lastname[i];            
 
-    //promedio bajo 
+                //enter calification from student by input
+                cout << "\tenter calification from student " << endl;
+                for (int i = 0; i <= 4; i++)
+                {
+                    cout << "\t\tEnter " << listMaterias[i] << " : ";
+                    cin >> calificaciones[i];
+                    sumaNotas += calificaciones[i];
+                    promedioAlumno = sumaNotas / 5;
+                    //el ultimo posicion es para el promedio del alumno
+                    if (i == 4)
+                    {
+                        listNotes[j] = promedioAlumno;
+                        /*sumaCalificacionPromedioAlumno += listNotes[j];
+                        promedioGrupo = sumaCalificacionPromedioAlumno / 20;*/
+                        promedioAlumno = 0.0;
+                        sumaNotas = 0.0;
+                        j++;
+                        
+                    }
+                }
+                //validar datos correctos
+                cout << "Los datos son correctos? S/N: ";
+                cin >> respuesta;
+                if (respuesta == 'n')
+                {
+                    j = j-1;
+                    //double listNotes[20] = {};
+                    /*sumaCalificacionPromedioAlumno = 0.0;
+                    promedioGrupo = 0.0;*/
+
+                }
+            } while (respuesta != 'y' && respuesta != 'Y' && respuesta != 's' && respuesta != 'S');
+        }
+
+    ////Se valida las notas altas y bajas
     int aux = 0;
     int aux1 = 0;
     lowNote = listNotes[0];
-    for (int  i = 0; i < 3; i++)
+    for (int  i = 0; i < 20; i++)
     {
         if (listNotes[i] > hightNote)
         {
             hightNote = listNotes[i];
-            aux++;
+            aux = i + 1;
         }
 
         if (listNotes[i] < lowNote)
         {
             lowNote = listNotes[i];
-            aux1++;
+            aux1 = i + 1;
         }
     }
 
 
     //tomar el promecdio de alumno guardarlo en la lista de promedio alumnos
     
-    cout << "list Notes " << endl;
-    for (int i = 0; i < 3; i++)
+    cout << "\nlist Students " << endl;
+
+    for (int i = 0; i < 20; i++)
     {
-        cout << listNotes[i] << endl;
+        cout <<i+1<<"\t"<< name[i] << " " << lastname[i] <<"\t"<< listNotes[i] << endl;
+        sumaCalificacionPromedioAlumno += listNotes[i];
+        promedioGrupo = sumaCalificacionPromedioAlumno / 20;
     }
 
-    
-    cout << "Promedio Grupal " << promedioGrupo << endl
-        << "Studnet: " << name[aux] << " " << lastname[aux] << "     HightNote: " << hightNote
-        << "\nstudent: " << name[aux1] << " " << lastname[aux1] << "      lowNote: " << lowNote << endl;
+    //muestra los datos     
+    cout << "\nPromedio Grupal " << promedioGrupo << endl
+        << "Studnet: " << aux << "\t:" << name[aux-1] << " " << lastname[aux-1] << "\tHightNote: " << hightNote
+        << "\nstudent: " << aux1 << "\t:" << name[aux1-1] << " " << lastname[aux1-1] << "\tlowNote: " << lowNote << endl;
 
     return 0;
 }
